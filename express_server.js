32f14eca03d485110773b43e5b2af7d47e5ca3b4
shangguanwei <coopershang125@gmail.com>
2020-09-29 00:46:49 +0000

    create a URL submission form

--- express_server.js
+++ express_server.js
@@ -9,22 +9,22 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com',
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render('urls_show', templateVars);
 });
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
-app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
-  };
-  res.render('urls_show',templateVars);
-});
-
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
@@ -33,6 +33,9 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}! `);
 });
